b8c1b1e3752289fe0f774e0cc60d5930f776bbf8
band-ald1107 <150985329+band-ald1107@users.noreply.github.com>
2024-11-22 20:12:19 +0800

    Update module_9(2).py

--- module_9(2).py
+++ module_9(2).py
@@ -1,45 +1,21 @@
-def calculate_sum_and_length(data):
-    total_sum = 0
-    total_length = 0
-
-    if isinstance(data, dict):
-        for key, value in data.items():
-            if isinstance(key, (int, float)):
-                total_sum += key
-            elif isinstance(key, str):
-                total_length += len(key)
-            total_sum, length = calculate_sum_and_length(value)
-            total_sum += length
-
-    elif isinstance(data, list):
-        for item in data:
-            total_sum_item, length = calculate_sum_and_length(item)
-            total_sum += total_sum_item
-            total_length += length
-
-    elif isinstance(data, tuple):
-        for item in data:
-            total_sum_item, length = calculate_sum_and_length(item)
-            total_sum += total_sum_item
-            total_length += length
-
-    elif isinstance(data, (int, float)):
-        total_sum += data
-
-    elif isinstance(data, str):  
-        total_length += len(data)
-
-    return total_sum, total_length
+def calculate_structure_sum(data_structure):
+    summa = 0
+    if isinstance(data_structure, dict):
+        for i in dict.items(data_structure):
+            summa += calculate_structure_sum(i)
+    elif isinstance(data_structure, (list, tuple, set)):
+        for i in data_structure:
+            summa += calculate_structure_sum(i)
+    elif isinstance(data_structure, int):
+        summa += data_structure
+    elif isinstance(data_structure, str):
+        summa += len(data_structure)
+    return summa
 
 
-data_structure = [
-    [1, 2, 3],
-    {'a': 4, 'b': 5},
-    (6, {'cube': 7, 'drum': 8}),
-    "Hello",
-    ((), [{(2, 'Urban', ('Urban2', 35))}])
-]
+data_structure = [[1, 2, 3], {'a': 4, 'b': 5}, (6, {'cube': 7, 'drum': 8}),
+                  "Hello", ((), [{(2, 'Urban', ('Urban2', 35))}])]
 
-total_sum, total_length = calculate_sum_and_length(data_structure)
+result = calculate_structure_sum(data_structure)
+print(result)
 
-print(f"Сумма всех чисел: {total_sum}, Общая длина всех строк: {total_length}")
